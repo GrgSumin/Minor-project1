f74d2eae7ef45d5dd08a5ec3ee1af7ebe66c3d48
GrgSumin <be2019se673@gces.edu.np>
2022-07-09 19:29:06 +0545

    removed

--- php/php/databaseconnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "movie_ticket_system";
-
-$connection = mysqli_connect($servername, $username, $password, $dbname);
